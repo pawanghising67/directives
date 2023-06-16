3fe7fe1e79d9d43472dca7fab3a4be2eac15a09e
[Luna Shrestha] <[luna.shrestha@mnsu.edu]>
2023-06-15 23:03:09 -0500

    first commit, practicing ngDirectives

---
+++ src/app/if/if.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if',
+  templateUrl: './if.component.html',
+  styleUrls: ['./if.component.css']
+})
+export class IfComponent implements OnInit {
+  x: number;
+  y: number;
+  s: string;
+  constructor() { 
+    this.x= 10;
+    this.y= 20;
+    this.s = "Hello"
+  }
+  myFun(){
+    return true;
+  }
+  ngOnInit(): void {
+  }
+
+}
